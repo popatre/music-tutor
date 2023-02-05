fb628e7a3d612b21da51fdf993a954cff0ac7a63
popatre <76402245+popatre@users.noreply.github.com>
2023-02-05 22:59:23 +0000

    add key click styling

--- pages/piano-player.tsx
+++ pages/piano-player.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import styles from "@/styles/Piano.module.css";
 import useSound from "use-sound";
 
@@ -18,72 +18,99 @@ export default function PianoPlayer() {
     const [playG] = useSound("/audio/G.mp3");
     const [playGb] = useSound("/audio/Gb.mp3");
 
+    const keyRef = useRef<HTMLDivElement>(null);
+
+    const [keyPressed, setKeyPressed] = useState({
+        C: "",
+        Db: "",
+        D: "",
+        E: "",
+        Eb: "",
+        F: "",
+        G: "",
+        Gb: "",
+        A: "",
+        Ab: "",
+        Bb: "",
+        B: "",
+    });
+
     const handleClick = (note: string) => {
         eval(`play${note}()`);
+
+        setKeyPressed((prevState) => {
+            return { ...prevState, [note]: styles.active };
+        });
+        setTimeout(() => {
+            setKeyPressed((prevState) => {
+                return { ...prevState, [note]: "" };
+            });
+        }, 200);
     };
 
     return (
         <main className={styles.container}>
             <div className={styles.piano}>
                 <div
+                    ref={keyRef}
                     data-note="C"
-                    className={`${styles.key} ${styles.white}`}
+                    className={`${styles.key} ${styles.white} ${keyPressed.C}`}
                     onClick={() => handleClick("C")}
                 ></div>
                 <div
                     onClick={() => handleClick("Db")}
                     data-note="Db"
-                    className={`${styles.key} ${styles.black}`}
+                    className={`${styles.key} ${styles.black} ${keyPressed.Db}`}
                 ></div>
                 <div
                     onClick={() => handleClick("D")}
                     data-note="D"
-                    className={`${styles.key} ${styles.white}`}
+                    className={`${styles.key} ${styles.white} ${keyPressed.D}`}
                 ></div>
                 <div
                     onClick={() => handleClick("Eb")}
                     data-note="Eb"
-                    className={`${styles.key} ${styles.black}`}
+                    className={`${styles.key} ${styles.black} ${keyPressed.Eb}`}
                 ></div>
                 <div
                     onClick={() => handleClick("E")}
                     data-note="E"
-                    className={`${styles.key} ${styles.white}`}
+                    className={`${styles.key} ${styles.white} ${keyPressed.E}`}
                 ></div>
                 <div
                     onClick={() => handleClick("F")}
                     data-note="F"
-                    className={`${styles.key} ${styles.white}`}
+                    className={`${styles.key} ${styles.white} ${keyPressed.F}`}
                 ></div>
                 <div
                     onClick={() => handleClick("Gb")}
                     data-note="Gb"
-                    className={`${styles.key} ${styles.black}`}
+                    className={`${styles.key} ${styles.black} ${keyPressed.Gb}`}
                 ></div>
                 <div
                     onClick={() => handleClick("G")}
                     data-note="G"
-                    className={`${styles.key} ${styles.white}`}
+                    className={`${styles.key} ${styles.white} ${keyPressed.G}`}
                 ></div>
                 <div
                     onClick={() => handleClick("Ab")}
                     data-note="Ab"
-                    className={`${styles.key} ${styles.black}`}
+                    className={`${styles.key} ${styles.black} ${keyPressed.Ab}`}
                 ></div>
                 <div
                     onClick={() => handleClick("A")}
                     data-note="A"
-                    className={`${styles.key} ${styles.white}`}
+                    className={`${styles.key} ${styles.white} ${keyPressed.A}`}
                 ></div>
                 <div
                     onClick={() => handleClick("Bb")}
                     data-note="Bb"
-                    className={`${styles.key} ${styles.black}`}
+                    className={`${styles.key} ${styles.black} ${keyPressed.Bb}`}
                 ></div>
                 <div
                     onClick={() => handleClick("B")}
                     data-note="B"
-                    className={`${styles.key} ${styles.white}`}
+                    className={`${styles.key} ${styles.white} ${keyPressed.B}`}
                 ></div>
             </div>
             {/* <audio id="C" src="../audio/C.mp3"></audio>
