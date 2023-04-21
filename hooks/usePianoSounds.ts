a94b8381dc71625ec10e5fab00aed98d5fe31f0b
popatre <76402245+popatre@users.noreply.github.com>
2023-04-21 19:42:42 +0100

    add better audio sounds

--- hooks/usePianoSounds.ts
+++ hooks/usePianoSounds.ts
@@ -2,18 +2,18 @@ import useSound from "use-sound";
 import { PlayFunction } from "use-sound/dist/types";
 
 export default function usePianoSounds() {
-    const [playA, exposedDataA] = useSound("/audio/A.mp3");
-    const [playAb, exposedDataAb] = useSound("/audio/Ab.mp3");
-    const [playB, exposedDataB] = useSound("/audio/B.mp3");
-    const [playBb, exposedDataBb] = useSound("/audio/Bb.mp3");
-    const [playC, exposedDataC] = useSound("/audio/C.mp3");
-    const [playD, exposedDataD] = useSound("/audio/D.mp3");
-    const [playDb, exposedDataDb] = useSound("/audio/Db.mp3");
-    const [playE, exposedDataE] = useSound("/audio/E.mp3");
-    const [playEb, exposedDataEb] = useSound("/audio/Eb.mp3");
-    const [playF, exposedDataF] = useSound("/audio/F.mp3");
-    const [playG, exposedDataG] = useSound("/audio/G.mp3");
-    const [playGb, exposedDataGb] = useSound("/audio/Gb.mp3");
+    const [playA, exposedDataA] = useSound("/audio/A.wav");
+    const [playAb, exposedDataAb] = useSound("/audio/Ab.wav");
+    const [playB, exposedDataB] = useSound("/audio/B.wav");
+    const [playBb, exposedDataBb] = useSound("/audio/Bb.wav");
+    const [playC, exposedDataC] = useSound("/audio/C.wav");
+    const [playD, exposedDataD] = useSound("/audio/D.wav");
+    const [playDb, exposedDataDb] = useSound("/audio/Db.wav");
+    const [playE, exposedDataE] = useSound("/audio/E.wav");
+    const [playEb, exposedDataEb] = useSound("/audio/Eb.wav");
+    const [playF, exposedDataF] = useSound("/audio/F.wav");
+    const [playG, exposedDataG] = useSound("/audio/G.wav");
+    const [playGb, exposedDataGb] = useSound("/audio/Gb.wav");
 
     const keyMap: { [key: string]: string } = {
         a: "C",
