f358a7baa63f81041c67cd34210fd523b44d4eb9
Popatre <jonny_m111@hotmail.com>
2023-06-29 11:54:48 +0100

    add centering css

--- pages/melody-maker.tsx
+++ pages/melody-maker.tsx
@@ -1,17 +1,19 @@
 import MelodyLab from "../components/MelodyLab/Melodylab";
+import styles from "../components/MelodyLab/MelodyMachine.module.scss";
 
 export default function Melodymaker() {
     return (
-        <div>
+        <div className={styles.container}>
+            <h1>Melody Maker</h1>
             <MelodyLab
                 samples={[
-                    { url: "/audio/B.wav", name: "B" },
-                    { url: "/audio/A.wav", name: "A" },
-                    { url: "/audio/G.wav", name: "G" },
-                    { url: "/audio/F.wav", name: "F" },
-                    { url: "/audio/E.wav", name: "E" },
-                    { url: "/audio/D.wav", name: "D" },
-                    { url: "/audio/C.wav", name: "C" },
+                    { url: "/audio/B.wav", name: "" },
+                    { url: "/audio/A.wav", name: "" },
+                    { url: "/audio/G.wav", name: "" },
+                    { url: "/audio/F.wav", name: "" },
+                    { url: "/audio/E.wav", name: "" },
+                    { url: "/audio/D.wav", name: "" },
+                    { url: "/audio/C.wav", name: "" },
                 ]}
             />
         </div>
